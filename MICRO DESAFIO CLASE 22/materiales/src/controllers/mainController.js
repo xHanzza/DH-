f01de43589c5543a6bc590816dba115d8c228034
xHanzza <cortegosogonzalo@gmail.com>
2022-07-22 21:43:22 -0300

    subiendo ejerecio

---
+++ MICRO DESAFIO CLASE 22/materiales/src/controllers/mainController.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		
+		res.render("index", {products});
+	},
+	search: (req, res) => {
+		const searchWord = req.query.keywords;
+		const productResult = products.filter((item) => {
+			return item.name.toLowerCase().includes(searchWord);
+		}) 
+		res.render("results", {products});
+	},
+
+};
+
+module.exports = controller;
